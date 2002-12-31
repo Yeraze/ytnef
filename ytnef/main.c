3b7cbcd5bd11c94d76d1ae99f47253be7d3136ee
yerase <>
2002-12-31 04:26:06 +0000

    Version # update.

--- ytnef/main.c
+++ ytnef/main.c
@@ -10,7 +10,7 @@ TNEFStruct TNEF;
 
 void PrintHelp(void) {
 
-    printf("Yerase TNEF Exporter v1.01\n");
+    printf("Yerase TNEF Exporter v1.02\n");
     printf("\n");
     printf("  usage: ytnef [-+vhf] <filenames>\n");
     printf("\n");
