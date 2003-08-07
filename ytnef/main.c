446ae2b9af3da78f1958131a0fbd34e1ad2c3ba2
yerase <>
2003-08-07 02:50:02 +0000

    V2.0 Cvs, now links to libytnef.

--- ytnef/main.c
+++ ytnef/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "tnef.h"
+#include <ytnef.h>
 #include "config.h"
 
 #define PRODID "PRODID:-//The Gauntlet//" PACKAGE_STRING "//EN\n"
