f80d47211e0dadb643687bba847cff4a54063b0d
yerase <>
2005-06-24 18:42:24 +0000

    Merged a patch from 'sridhar_sds', fixes a memory leak in TNEFFreeMapiProps

--- ytneflib/ytnef.c
+++ ytneflib/ytnef.c
@@ -889,6 +889,10 @@ void TNEFFreeMapiProps(MAPIProps *p)
             FREEVARLENGTH(p->properties[i].data[j]);
         }
         free(p->properties[i].data);
+        for(j=0;j<p->properties[i].namedproperty;j++) {
+            FREEVARLENGTH(p->properties[i].propnames[j]);
+        }
+        free(p->properties[i].propnames);
     }
     free(p->properties);
     p->count = 0;
