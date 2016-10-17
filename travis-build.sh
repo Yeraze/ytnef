871f71dc14ea4aad301848364a30d56b0780707a
Randall Hand <randall.hand@gmail.com>
2016-10-17 09:20:11 -0400

    Added `--disable-dependency-tracking` to configure

--- travis-build.sh
+++ travis-build.sh
@@ -2,12 +2,12 @@
 set -ev
 cd ytneflib
 ./autogen.sh
-./configure
+./configure --disable-dependency-tracking
 make
 sudo make install
 cd ../ytnef
 ./autogen.sh
-./configure 
+./configure --disable-dependency-tracking
 make
 
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
