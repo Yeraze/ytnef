9b84a563e96bb103fc0cd06c6112b3ca3f495322
Randall Hand <randall.hand@gmail.com>
2017-02-23 14:29:48 -0500

    Add address sanitization to Travis build
    To detect future memory bugs, make this a standard part of the Travis
    CI run.

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@ set -ev
 mkdir -p m4
 autoreconf -vfi
 ./configure --disable-dependency-tracking
-make
+make CFLAGS="-fsanitize=address -g"
 sudo make install
 
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
