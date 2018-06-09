1a7f49fff2971dc423671c2c391b13f13bb7e357
Oliver Giles <ohw.giles@gmail.com>
2018-06-09 09:31:46 +0300

    IsCompressedRTF: fix incorrect size check

--- lib/ytnef.c
+++ lib/ytnef.c
@@ -1489,23 +1489,16 @@ void MAPIPrint(MAPIProps *p) {
 
 
 int IsCompressedRTF(variableLength *p) {
-  unsigned int in;
   BYTE *src;
   ULONG magic;
 
-  if (p->size < 4)
+  if (p->size < 3*sizeof(DWORD))
     return 0;
 
   src = p->data;
-  in = 0;
+  magic = SwapDWord((BYTE*)src + 2*sizeof(DWORD), sizeof(DWORD));
 
-  in += 4;
-  in += 4;
-  magic = SwapDWord((BYTE*)src + in, 4);
-
-  if (magic == 0x414c454d) {
-    return 1;
-  } else if (magic == 0x75465a4c) {
+  if (magic == 0x414c454d || magic == 0x75465a4c) {
     return 1;
   } else {
     return 0;
